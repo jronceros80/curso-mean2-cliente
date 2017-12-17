8b22e06ad68a38492230d2cf7febfa1941c8fdc6
jronceros80 <jronceros80@gmail.com>
2017-12-17 19:20:12 +0100

    refact2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,15 @@ import { HttpModule } from '@angular/http';
 import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { UserEditComponent } from './components/user-edit.component';
-import { ArtistListComponent } from './components/artist-list.component';
-import { HomeComponent } from './components/home.component';
-import { ArtistAddComponent } from './components/artist-add.component';
-import { ArtistEditComponent } from './components/artist-edit.component';
-import { ArtistDetailComponent } from './components/artist-detail.component';
 import { HijoComponent } from './components/hijo.component';
 import { InputNumberComponent } from './components/input-number.component';
-
-
-=======
 import { UserEditComponent } from './components/user/user-edit.component';
 import { ArtistListComponent } from './components/artist/artist-list.component';
 import { HomeComponent } from './components/home/home.component';
 import { ArtistAddComponent } from './components/artist/artist-add.component';
 import { ArtistEditComponent } from './components/artist/artist-edit.component';
 import { ArtistDetailComponent } from './components/artist/artist-detail.component';
->>>>>>> 1bd61c3... refactorizacion
+
 
 @NgModule({
   declarations: [
